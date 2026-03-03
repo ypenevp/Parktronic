95a0a8cfd21280419f8a52751e7e59dd6f4e222b
Bobi-77 <borislavstojnev8@gmail.com>
2026-03-03 16:52:20 +0200

    feat(Hardware): soundIndication add

--- Hardware/src/main.cpp
+++ Hardware/src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 
-void setup() {
+#define BUZZER 5
 
+void setup() {
+    Serial.begin(115200);
+    pinMode(BUZZER, OUTPUT);
 }
 
 void lightIndication(float distance){
@@ -9,9 +12,26 @@ void lightIndication(float distance){
 }
 
 void soundIndication(float distance){
-
+    if(distance > 20){
+        noTone(BUZZER);
+    } else if (distance <= 20 && distance > 13){
+        tone(BUZZER, 500, 100);
+        delay(900);
+    } else if(distance <= 13 && distance > 6){
+        tone(BUZZER, 1000, 100);
+        delay(400);
+    } else {
+        tone(BUZZER, 2000);
+    }
 }
 
 void loop() {
-
+    soundIndication(25);
+    delay(2000);
+    soundIndication(18);
+    delay(2000);
+    soundIndication(10);
+    delay(2000);
+    soundIndication(3);
+    delay(2000);
 }
