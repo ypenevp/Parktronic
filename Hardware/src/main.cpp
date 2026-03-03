634e1c4b2c39bb0613ed6efc92d79dd20716ac5f
ypenevp <ypenevp@gmail.com>
2026-03-03 17:07:36 +0200

    feat(Hardware): lightIndication add

--- Hardware/src/main.cpp
+++ Hardware/src/main.cpp
@@ -2,13 +2,45 @@
 
 #define BUZZER 5
 
+#define GREEN_LED 35
+#define YELLOW_LED 36
+#define RED_LED 37
+
 void setup() {
     Serial.begin(115200);
     pinMode(BUZZER, OUTPUT);
+
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(YELLOW_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
 }
 
-void lightIndication(float distance){
-
+void lightIndication(float distance)
+{
+  if (distance > 20)
+  {
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+  }
+  else if (distance >= 13 && distance <= 20)
+  {
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+  }
+  else if (distance >= 6 && distance <= 13)
+  {
+    digitalWrite(YELLOW_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+  }
+  else if (distance >= 0 && distance <= 6)
+  {
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+  }
 }
 
 void soundIndication(float distance){
@@ -26,12 +58,20 @@ void soundIndication(float distance){
 }
 
 void loop() {
-    soundIndication(25);
-    delay(2000);
     soundIndication(18);
     delay(2000);
-    soundIndication(10);
+    // soundIndication(18);
+    // delay(2000);
+    // soundIndication(10);
+    // delay(2000);
+    // soundIndication(3);
+    // delay(2000);
+    lightIndication(25);
     delay(2000);
-    soundIndication(3);
+    lightIndication(18);
+    delay(2000);
+    lightIndication(10);
+    delay(2000);
+    lightIndication(3);
     delay(2000);
 }
